e4243892aedd7d1a084420d5341d9ef35cbac384
fayod7 <fayodnv@gmail.com>
2025-05-04 22:31:11 +0500

    7

---
+++ js/app.js
@@ -0,0 +1,82 @@
+{
+    //M-1
+    // function repeat(word){
+    //     return word.repeat(3)
+    // }
+    // console.log( repeat("hello") );
+}
+{
+    //M-2
+    // function divide(str){
+    //     return str.slice(0,5)
+    // }
+    // console.log( divide("Salom dunyo") );
+    
+}
+{
+    //M-3
+    // function hasWord(word){
+    //     return word.includes("7")
+    // }
+    // console.log( hasWord("salom f7") );
+    
+}
+{
+    //M-4
+    // function findCharIndex(str, char){
+    //     return str.indexOf(char)
+    // }
+    // console.log( findCharIndex("Sa7lom", "7") );
+    
+}
+{
+    //M-5
+    // function replace(str){
+    //     return str.replace("bugun", "ertaga")
+    // }
+    // console.log( replace("bugun dars qilaman") );
+    
+}
+{
+    //M-6
+    // function upper(str){
+    //     return str.slice(-3).toUpperCase();
+    // }
+    // console.log( upper("salom") );
+    
+}
+{
+    //M-7
+    // function divide(str){
+    // let result = str.split(" ")
+    // return result
+    // }
+    // console.log( divide("Hello world helloworld") );
+}
+{
+    //M-8
+    // function tol(str){
+    //     return str.padStart(4, "0").repeat(2)
+    // }
+    // console.log( tol("7") );
+    
+}
+{
+    //M-9
+    // function divide(str){
+    //     return str.replace(/-/g, " ").split(" ").join(" ")
+    // }
+    // console.log( divide("Hello-world-7") );
+    
+}
+{
+    //M-10
+    // function replaceUsername(email){
+    //     let at = email.indexOf("@")
+    //     return `user${email.slice(at)}`
+    // }
+    // console.log( replaceUsername("fayod@gmail.com") );
+    
+}
+
+
