032d74df785a5c00a4376bf6c85eca08dd7e23af
fayod7 <fayodnv@gmail.com>
2025-05-05 05:41:45 +0500

    07

--- js/app.js
+++ js/app.js
@@ -4,11 +4,29 @@
     //     return word.repeat(3)
     // }
     // console.log( repeat("hello") );
+
+    // function repeat(word){
+    //     let result = ""
+    //    for( let i = 0; i < 3; i ++ ){
+    //     result += word
+    //    }
+    //    return result
+    // }
+    // console.log( repeat("hello") );
 }
 {
     //M-2
     // function divide(str){
     //     return str.slice(0,5)
+    // }
+    // console.log( divide("Salom dunyo") );
+
+    // function divide(str){
+    //     let result = ""
+    //     for( let i = 0; i < 5; i ++ ){
+    //         result += str[i]
+    //     }
+    //     return result
     // }
     // console.log( divide("Salom dunyo") );
     
@@ -19,12 +37,29 @@
     //     return word.includes("7")
     // }
     // console.log( hasWord("salom f7") );
+
+    // function hasWord(word){
+    //     for( let i = 0; i < word.length; i ++ ){
+    //             return word[i] === "7" ? false : true
+    //     }
+    // }
+    // console.log( hasWord("salom f7") );
     
 }
 {
     //M-4
     // function findCharIndex(str, char){
     //     return str.indexOf(char)
+    // }
+    // console.log( findCharIndex("Sa7lom", "7") );
+
+
+    // function findCharIndex(str, char){
+    //     for( let i = 0; i < str.length; i ++ ){
+    //         if( str[i] === "7" ){
+    //             return i
+    //         }
+    //     }
     // }
     // console.log( findCharIndex("Sa7lom", "7") );
     
